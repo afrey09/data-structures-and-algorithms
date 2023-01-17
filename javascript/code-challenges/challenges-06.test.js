663c9162b8619648a3a7da77f896633a939fe2b7
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-01-16 19:46:57 -0800

    problem 1 and 2

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -38,6 +38,8 @@ CHALLENGE 2
 
 Write a function that appends ' The end.' to a string, and returns the modified string. The original source string should not be modified.
 
+!example found in class repo ReadMe
+
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => str + ' The end.';
@@ -56,9 +58,7 @@ appendFirstToLast(a);
 console.log(a) prints [1, 2, 3, 1]
 ------------------------------------------------------------------------------------------------ */
 
-const appendFirstToLast = (arr) => {
-  // Solution code here...
-};
+const appendFirstToLast = (arr) => 
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
