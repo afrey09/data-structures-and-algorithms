5b4c2e3c802c66fde7a1c9e76b263ffa8fcb734d
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-02-19 14:49:20 -0800

    code challenge 5 complete

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,50 @@
 'use strict';
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insert(value) {
+    const oldHead = this.head;
+
+    this.head = new Node(value);
+    this.head.next = oldHead;
+  }
+
+  toString() {
+    let current = this.head;
+    let str = '';
+
+    while (current) {
+      str += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+
+    return str += 'NULL';
+  }
+
+  includes(value) {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+
+      current = current.next;
+    }
+
+    return false;
+
+  }
+}
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 module.exports = LinkedList;
