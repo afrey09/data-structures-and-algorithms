ed2ace2e7ffae3b5ce9d2138e766dd11f2efd232
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-03-05 19:08:27 -0800

    preorder method

--- javascript/Trees/index.js
+++ javascript/Trees/index.js
@@ -15,7 +15,16 @@ class BinaryTree {
   }
 
   preOrder() {
+    const data = [];
+    let current = this.root;
+
+    const traverse = (node) => {
+      data.push(node.value);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
   }
+    traverse(current);
+    return data;
 
 
   inOrder() {
