4c2d5c7288ef104836c1ae79ffa5f1d5c9719593
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-03-05 18:20:09 -0800

    initial structure setup

---
+++ javascript/Trees/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+
+  }
+
+  preOrder() {
+  }
+
+
+  inOrder() {
+  }
+
+  postOrder() {
+  }
+}
+
+
+class BinarySearchTree {
+  constructor() {
+
+  }
+}
+
+module.exports = { Node, BinaryTree, BinarySearchTree };
